f44e6fbcac62554a0583675d411bd8a4bb2edbf2
Abhinav Sancheti <001sancheti@gmail.com>
2021-09-08 18:39:58 +0530

    created initial screens and installed dependencies

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import { FlatList, Text, Image, View, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
 
 const data = [
     {
@@ -20,10 +22,32 @@ const NavOptions = () => {
     return (
         <FlatList
             data={data}
+            horizontal
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-                <TouchableOpacity>
-                    <Text>{item.title}</Text>
+                <TouchableOpacity
+                    style={tw`p-2 pl-6 pb-8 bg-gray-200 m-2 w-40`}
+                >
+                    <View>
+                        <Image
+                            style={{
+                                width: 120,
+                                height: 120,
+                                resizeMode: 'contain',
+                            }}
+                            source={{ uri: item.image }}
+                        />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>
+                            {item.title}
+                        </Text>
+                        <Icon
+                            type='antdesign'
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            name='arrowright'
+                            color='white'
+                        />
+                        
+                    </View>
                 </TouchableOpacity>
             )}
         />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
 
